0b85c2f58e74271ef59ae000764724ee9b23f9d7
andrey0902 <andreypavlov1706@gmail.com>
2018-08-07 10:50:04 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  color = environment.backGround;
+
+  public testString(str, end: string): boolean {
+    return (<string>str).endsWith(end);
+  }
+
+  public test() {
+
+  }
+
+  test2() {
+
+  }
 }
